22d207ffd27dd6cb07327be463269ca6fc6d3f50
Dani JS <levi@JS.local>
2023-06-27 20:53:41 +0630

    Styling Web v1

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(express.static("public"))
 app.get("/", (req, res) => {
-    res.send("Hello World..")
+    res.sendFile(__dirname + "/index.html")
 })
 
 
